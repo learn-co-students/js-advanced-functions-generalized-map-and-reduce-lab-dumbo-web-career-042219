15516b6b45a73261c2317a3af6b230f5f050a0ad
Miles Marillo <mjm03@hampshire.edu>
2019-06-13 12:19:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+const map = (testArray, callBack) => {
+	let mappedArray = [];
+		let i;
+			for(i = 0; i < testArray.length; i++) {
+				mappedArray.push(callBack(testArray[i]))
+					}
+						return mappedArray
+}
+
+const reduce = (testArray, callBack, startingPoint = 0) => {
+	let reducedTotal = (!!startingPoint) ? startingPoint : testArray[0]
+		let i = (!!startingPoint) ? 0 : 1
+			for(; i < testArray.length; i++) {
+				reducedTotal = callBack(testArray[i], reducedTotal);
+					}
+						return reducedTotal
+}
