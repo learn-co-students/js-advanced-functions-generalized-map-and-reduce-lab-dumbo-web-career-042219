d3c76bcaf1c1208163b779a8ce3fd5817d20a8d7
Wei <theweifeng@gmail.com>
2019-06-13 17:27:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+const map = (arr, action) => {
+  let new_arr = [];
+  arr.forEach(ele => {
+    new_arr.push(action(ele));
+  });
+  return new_arr;
+};
+
+const reduce = (arr, action, starting_value) => {
+  let reduced;
+  
+  if (starting_value){
+    reduced = starting_value;
+  } else {
+    reduced = arr.shift();
+  }
+  arr.forEach(ele => {
+    reduced = action(ele, reduced);
+  });
+
+  return reduced;
+}
