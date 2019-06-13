8552d3034df2cd47b164b8f309accc2ad5a42300
Aaron Chu <aaronchu357@gmail.com>
2019-06-13 15:08:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+let map = function(arr, fn) {
+  return arr.map(num => {
+    return fn(num)
+  })
+}
+
+function reduce(arr, fn, sp){
+  let r = (!!sp) ? sp : arr[0]
+  let i = (!!sp) ? 0 : 1
+
+  for (; i < arr.length; i++) {
+    r = fn(arr[i], r)
+  }
+
+  return r;
+}
