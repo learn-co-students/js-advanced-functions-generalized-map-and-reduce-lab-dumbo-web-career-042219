8c60bd20fba1b613e1a8bfa86156a261b6cbfae8
sebastosh <sebastosh@gmail.com>
2019-06-23 21:58:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(source, action) {
+  let array = []
+
+  for (let i = 0; i < source.length; i++) {
+    let item = source[i]
+    array.push(action(item))
+  }
+
+  return array;
+}
+
+function reduce(source, action, starting){
+  let array = (!!starting) ? starting : source[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < source.length; i++) {
+    array = action(source[i], array)
+  }
+
+  return array;
+}
